63393ab129a54bc1bb4167054f5ae8f2715f91b5
unknown <mousscha606@gmail.com>
2024-08-06 12:21:21 +0100

    privacy and policy added

--- screens/Profile.js
+++ screens/Profile.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
+  ScrollView,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
@@ -21,9 +22,11 @@ import { Userctx } from "../store/Usercontext";
 import { ipadresse } from "../config";
 import axios from 'axios';
 
+
 const Profile = () => {
   const Userinfo = useContext(Userctx);
   const [isModalVisible, setModalVisible] = useState(false);
+  const [isPrivacyModalVisible, setPrivacyModalVisible] = useState(false);
   const [name, setName] = useState(Userinfo.username);
   const [phone, setPhone] = useState(Userinfo.phone);
   const [isSaveDisabled, setIsSaveDisabled] = useState(true);
@@ -35,6 +38,10 @@ const Profile = () => {
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
+  };
+
+  const togglePrivacyModal = () => {
+    setPrivacyModalVisible(!isPrivacyModalVisible);
   };
 
   const handleSave = () => {
@@ -121,7 +128,11 @@ const Profile = () => {
           </View>
           <View style={styles.sp}></View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonSection} activeOpacity={0.9}>
+        <TouchableOpacity 
+          style={styles.buttonSection} 
+          activeOpacity={0.9}
+          onPress={togglePrivacyModal}
+        >
           <View style={styles.buttonArea}>
             <View style={styles.imageArea}>
               <Image
@@ -192,6 +203,84 @@ const Profile = () => {
               <Text style={styles.modalButtonText}>Cancel</Text>
             </TouchableOpacity>
           </View>
+        </View>
+      </Modal>
+      <Modal
+        isVisible={isPrivacyModalVisible}
+        onBackdropPress={togglePrivacyModal}
+        style={styles.modal}
+      >
+        <View style={styles.modalBackground}>
+        <View style={styles.modalContent}>
+        <ScrollView 
+          contentContainerStyle={styles.modalContentContainerStyle}
+        >
+          <Text style={styles.modalTitle}>Privacy Policy</Text>
+          <Text style={styles.privacyText}>
+            Last updated: August 06, 2024
+          </Text>
+          <Text style={styles.privacyText}>
+            This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
+          </Text>
+          <Text style={styles.privacyText}>
+            We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Free Privacy Policy Generator.
+          </Text>
+          <Text style={styles.privacyText}>
+            Interpretation and Definitions
+          </Text>
+          <Text style={styles.privacyText}>
+            The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.
+          </Text>
+          <Text style={styles.privacyText}>
+            For the purposes of this Privacy Policy:
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Account</Text> means a unique account created for You to access our Service or parts of our Service.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Affiliate</Text> means an entity that controls, is controlled by or is under common control with a party, where "control" means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Application</Text> refers to GymGuru, the software program provided by the Company.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Company</Text> (referred to as either "the Company", "We", "Us" or "Our" in this Agreement) refers to GymGuru.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Country</Text> refers to:  Tunisia
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Device</Text> means any device that can access the Service such as a computer, a cellphone or a digital tablet.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Personal Data</Text> is any information that relates to an identified or identifiable individual.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Service</Text> refers to the Application.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Service Provider</Text> means any natural or legal person who processes the data on behalf of the Company. It refers to third-party companies or individuals employed by the Company to facilitate the Service, to provide the Service on behalf of the Company, to perform services related to the Service or to assist the Company in analyzing how the Service is used.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>Usage Data</Text> refers to data collected automatically, either generated by the use of the Service or from the Service infrastructure itself (for example, the duration of a page visit).
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>You</Text> means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}>If you have any questions about this Privacy Policy, </Text>You can contact us:
+          </Text>
+          <Text style={styles.privacyText}>
+            <Text style={{ fontWeight: 'bold' }}> By email:</Text> GymGuru@gmail.com
+          </Text>
+        </ScrollView>
+        <TouchableOpacity
+          style={styles.closeButton}
+          onPress={togglePrivacyModal}
+        >
+          <Text style={styles.closeButtonText}>Close</Text>
+        </TouchableOpacity>
+        </View>
         </View>
       </Modal>
     </View>
@@ -287,11 +376,20 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     margin: 0,
   },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
   modalContent: {
     backgroundColor: "#2c2c2c",
     padding: 22,
     borderTopLeftRadius: 17,
     borderTopRightRadius: 17,
+    alignItems: "center",
+  },
+  modalContentContainerStyle: {
     alignItems: "center",
   },
   modalTitle: {
@@ -332,5 +430,19 @@ const styles = StyleSheet.create({
   modalButtonText: {
     color: "white",
     fontSize: 16,
+  },
+  privacyText: {
+    color: "white",
+    marginBottom: 10,
+  },
+  closeButton: {
+    backgroundColor: '#2196F3',
+    borderRadius: 5,
+    padding: 10,
+    alignItems: 'center',
+  },
+  closeButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
